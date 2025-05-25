3c2fbb0aeac83d2e81db51d4681dba4965b4d604
TinaKovalova <kovalevatkv@gmail.com>
2025-05-25 22:39:45 +0300

    data

--- js/index.js
+++ js/index.js
@@ -1,7 +1,47 @@
 "use strict";
 window.addEventListener("load", () => {
 
+    const createSlide = ({ title, imagePath, githubURL, projectURL, technologies }) => {
+      return ` <div class="swiper-slide">
+                                <article class="work-card">
+                                    <a href="${projectURL}" class="work-card__link">
+                                        <img src="${imagePath}" alt="The Tasteat landing"
+                                            class="work-card__image">
+                                    </a>
+                                    <div class="work-card__actions">
+                                        <a href="${githubURL}"
+                                            class="work-card__info-link _icon-github"></a>
+                                        <a href="${projectURL}"
+                                            class="work-card__info-link _icon-eye"></a>
+                                    </div>
+                                    <div class="work-card__info">
+                                        <h5 class="work-card__title">${title}</h5>
+                                        <div class="work-card__description">${technologies}</div>
+                                    </div>
+                                </article>
+                            </div>`;
+    };
 
-    
+    getData("../files/data.json");
+
+    async function getData(dataUrl) {
+        try {
+            const response = await fetch(dataUrl);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            const swiperWrapper = document.querySelector(".swiper-wrapper");
+            for (const project of json.projects) {
+                swiperWrapper.insertAdjacentHTML(
+                  "beforeend",
+                  createSlide(project)
+                );
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+        
+    }
     
 });
